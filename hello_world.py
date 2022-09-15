1ebbdacce15eae15c0411ff9d88d113cc69d2930
Venika Sem <venika.sem@mths.ca>
2022-09-15 15:07:36 +0000

    added hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Venika Sem
+# Created on: Sept 2020
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    # this function prints to the console
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
